5a56573f4faba21ad49ef29525e75aef3d43b203
MateuszWawrzynski <mateusz1799@gmail.com>
2021-09-21 13:11:57 +0200

    Design Faq mobile component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './App.module.scss'
 
+import useDesktopView from '../../hooks/useDesktopView';
+
 import Header from '../Header/Header'
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 import Gallery from '../Gallery/Gallery';
@@ -8,9 +10,13 @@ import BuyForm from '../BuyForm/BuyForm';
 import Versions from '../Versions/Versions';
 import Description from '../Description/Description';
 import Bestsellers from '../Bestsellers/Bestsellers';
+import FaqMobile from '../Faq/Mobile/FaqMobile';
+import FaqDesktop from '../Faq/Desktop/FaqDesktop';
 
 
 function App() {
+	const desktopMode = useDesktopView()
+
 	return (
 		<div className={styles.wrapper}>
 			<Header />
@@ -22,6 +28,38 @@ function App() {
 			</div>
 			<Description />
 			<Bestsellers titleShort='Bestsellery' titleLong='Zobacz podobne' />
+			{!desktopMode ? (
+				<div>
+					<FaqMobile title='Nasi klienci najczęściej pytali o:'>
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+					</FaqMobile>
+					<FaqMobile title='Opinie o miseczka bambusowa myszka dla dzieci'>
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+						Hello World!<br />
+					</FaqMobile>
+				</div>
+			) : (
+				<FaqDesktop />
+			)}
 		</div>
 	);
 }
---
+++ src/components/Faq/Desktop/FaqDesktop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './FaqDesktop.module.scss'
+
+
+export default function FaqDesktop() {
+	return (
+		<div className={styles.wrapper}>
+			
+		</div>
+	)
+}
